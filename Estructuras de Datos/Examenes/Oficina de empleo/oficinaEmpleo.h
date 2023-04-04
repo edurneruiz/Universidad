4f7cfb610facda8aa08d1f2cdd6bd0158da382f1
edurneruiz <129766740+edurneruiz@users.noreply.github.com>
2023-04-04 13:41:08 +0200

    Add files via upload

---
+++ Estructuras de Datos/Examenes/Oficina de empleo/oficinaEmpleo.h
@@ -0,0 +1,24 @@
+
+// NOMBRE Y APELLIDOS 
+
+// COMENTARIO SOBRE LA SOLUCIÓN
+// NO OLVIDES PONER EL COSTE JUSTIFICADO DE CADA OPERACIÓN JUNTO A ELLA
+
+#ifndef OFICINAEMPLEO
+#define OFICINAEMPLEO
+
+#include <iostream>               
+#include <string>
+#include <stdexcept>
+#include <algorithm>
+using namespace std;
+
+class oficinaEmpleo {
+   
+public:
+   
+};
+
+
+
+#endif
