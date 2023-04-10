4facc5fa69e71c0eecd37c7dbf154286294ce0ec
edurneruiz <129766740+edurneruiz@users.noreply.github.com>
2023-04-10 21:06:41 +0200

    Add files via upload

---
+++ Tecnología de la Programación/Space Invaders/src/game/PrinterTypes.java
@@ -0,0 +1,30 @@
+package tp2.game;
+
+
+public enum PrinterTypes {
+	BOARDPRINTER("boardprinter", "prints the game formatted as a board of dimension: ", new BoardPrinter()),
+	SERIALIZER("serializer", "prints the game as plain text", new Serializer());
+	private String printerName;
+	private String helpText;
+	private GamePrinter printerObject;
+	
+	private PrinterTypes(String name, String text, GamePrinter printer) {
+		printerName = name;
+		helpText = text;
+		printerObject = printer;
+	}
+	public static String printerHelp(Game game) {
+		String helpString = "";
+		for (PrinterTypes printer : PrinterTypes.values())
+		helpString += String.format(" %s : %s %s %n", printer.printerName, printer.helpText,(printer == BOARDPRINTER ? Game.DIM_X + " x " + Game.DIM_Y : "") );
+		return helpString;
+	}
+	public GamePrinter getObject(Game game) {
+		printerObject.setGame(game);
+		return printerObject;
+	}
+	
+	public String getPrinterName() { return printerName; }
+	public String getHelpText() { return helpText; }
+	public GamePrinter printerObject() { return printerObject; }
+}
